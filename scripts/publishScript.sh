d0e1634a33cf8b48337e391c05824dfef8f036e6
Maanuj Vora <maanujvora@gmail.com>
2020-09-30 15:54:56 -0700

    Update publishScript.sh

--- scripts/publishScript.sh
+++ scripts/publishScript.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-# if [ "`git status -s`" ]
-# then
-#     echo "The working directory is dirty. Please commit any pending changes."
-#     exit 1;
-# fi
+if [ "`git status -s`" ]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
 
 echo "Deleting old publication"
-rm -rf docs
-mkdir docs
+rm -rf public
+mkdir public
 git worktree prune
-rm -rf .git/worktrees/docs/
+rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B master docs origin/master
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
-rm -rf docs/*
+rm -rf public/*
 
 echo "Generating site"
 hugo
 
-# echo "Updating gh-pages branch"
-# cd public && git add --all && git commit -m "I am in charge 🗡️"
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "I am in charge 🗡️"
 
-# echo "Pushing to github"
-# git push --all
+echo "Pushing to github"
+git push --all
